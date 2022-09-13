87983f945fbc6b6bb3d119a810a99b5d5c051813
VAC <37587399+vacnex@users.noreply.github.com>
2022-09-13 20:07:21 +0700

    Clean space

--- scripts/example_spell_tracker.js
+++ scripts/example_spell_tracker.js
@@ -13,13 +13,13 @@ function onTick() {
 function onDraw() {
     const result = [];
 
-  const closest = manager.utils.enemyChampsInRange(2000, { includeClones: true, includeDead: true });
+    const closest = manager.utils.enemyChampsInRange(2000, { includeClones: true, includeDead: true });
 
-  ctx.textAt(closest.length, 40, 40, 22, 255);
+    ctx.textAt(closest.length, 40, 40, 22, 255);
 
-  if (closest.length == 0) return;
+    if (closest.length == 0) return;
 
-  ctx.circleAtPoint3D(closest[0].gamePos, 50, 150, 2, [200, 0, 0]);
+    ctx.circleAtPoint3D(closest[0].gamePos, 50, 150, 2, [200, 0, 0]);
 }
 
 register({ setup, onTick, onDraw });
